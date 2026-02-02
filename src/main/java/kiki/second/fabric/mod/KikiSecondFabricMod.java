f1948cf19c880a18106e2122c5ce103d3536aab3
Kiki-gm <kikidum2006@gmail.com>
2026-02-02 15:37:26 -0300

    Initial commit

---
+++ src/main/java/kiki/second/fabric/mod/KikiSecondFabricMod.java
@@ -0,0 +1,17 @@
+package kiki.second.fabric.mod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KikiSecondFabricMod implements ModInitializer {
+
+	public static final String MOD_ID = "kiki_second_fabric_mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/kiki/second/fabric/mod/KikiSecondFabricModClient.java
@@ -0,0 +1,11 @@
+package kiki.second.fabric.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KikiSecondFabricModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
